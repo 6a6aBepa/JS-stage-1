7cdcb17b75786b5d03bc68664f9c1f6e7fb34a09
Dmitrii <6a6abepa@bk.ru>
2020-02-02 01:26:25 +0300

    home work for 5 lession
    last commit and go to sleep

--- JS/shopJS.js
+++ JS/shopJS.js
@@ -1,8 +1,8 @@
 //Стыриный шоп
 
-let PRODUCTS_NAMES = ['Processor', 'Display', 'Notebook', 'Mouse', 'Keyboard']
-let PRICES = [100, 120, 1000, 15, 18]
-let IDS = [0, 1, 2, 3, 4]
+let PRODUCTS_NAMES = ['Processor', 'Display', 'Notebook', 'Mouse', 'Keyboard', 'Phones', 'Router', 'USB-camera', 'Gamepad']
+let PRICES = [100, 120, 1000, 15, 18, 25, 30, 18, 24]
+let IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 let products = []
 
@@ -21,6 +21,7 @@ function createProduct(index) {
         id_product: IDS[index]
     }
 }
+createProductsDTO()
 
 //Функционал корзины
 
@@ -30,12 +31,86 @@ function createProduct(index) {
 
 */
 
-let basket = [...products]                      //Должен быть в функции пдсчета стоимости
 
-function countBasketPrice() {                   //Работает после запуска createProductsDTO()
+//Работает после запуска createProductsDTO()
+function countBasketPrice() {
+    let basket = [...products] //Должен быть в функции пдсчета стоимости
     let sumBasket = 0
     for (let i = 0; i < products.length; i++) { //тут должна быть длина массива корзины
-        sumBasket += products[i].price          //тут подсчет стоимости товара в массиве корзины 
+        sumBasket += products[i].price //тут подсчет стоимости товара в массиве корзины 
     }
     return console.log(sumBasket)
 }
+createProductsDTO()
+
+function basket() {
+    let myBasket = []
+
+
+}
+
+let myProject = document.querySelector("#container")
+
+function header() {
+    let header = document.createElement("header")
+    let logo = document.createElement("a")
+    let formSearch = document.createElement("form")
+    let inputSearch = document.createElement("input")
+    let inputSubmit = document.createElement("input")
+    let basketHeade = document.createElement("button")
+
+    logo.classList.add("logo")
+    formSearch.classList.add("formSearch")
+    inputSearch.classList.add("inputSearch")
+    inputSubmit.classList.add("inputSubmit")
+    basketHeade.classList.add("basketHeader")
+
+    logo.setAttribute("href", " ")
+    inputSubmit.setAttribute("type", "submit")
+    inputSubmit.setAttribute("value", " ")
+    logo.innerText = "e-shop"
+    basketHeade.innerText = "Корзина"
+
+    header.appendChild(logo)
+    header.appendChild(basketHeade)
+    header.appendChild(formSearch)
+    formSearch.appendChild(inputSearch)
+    formSearch.appendChild(inputSubmit)
+    myProject.appendChild(header)
+}
+header()
+
+function catalog() {
+    let catalogAmount = PRODUCTS_NAMES.length
+    for (let i = 0; i < catalogAmount; i++) {
+        let div = document.createElement("div")
+        let img = document.createElement("img")
+        let p = document.createElement("p")
+        let a = document.createElement("a")
+        let buyButton = document.createElement("a")
+
+        div.classList.add("catalog-section")
+        p.classList.add("price")
+        img.classList.add("img-product")
+        a.classList.add("product-href")
+        buyButton.classList.add("buy")
+
+        a.innerText = PRODUCTS_NAMES[i]
+        a.setAttribute("href", " ")
+        buyButton.setAttribute("onclick", "basket()")
+        buyButton.setAttribute("href", " ")
+        p.innerHTML = PRICES[i] + " $"
+        img.setAttribute("src", "http://placehold.it/200x150")
+        buyButton.innerText = "Купить"
+
+        div.appendChild(img)
+        div.appendChild(a)
+        div.appendChild(p)
+        div.appendChild(buyButton)
+        myProject.appendChild(div)
+
+
+    }
+}
+
+catalog()
