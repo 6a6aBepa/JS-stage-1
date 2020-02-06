ec6a5aa47979a6933f08b992c1bb0e221819ad17
Dmitrii <6a6abepa@bk.ru>
2020-02-06 14:31:34 +0300

    last commit HW #6
    i did't understand how it work. Sorry(

--- JS/shopJS.js
+++ JS/shopJS.js
@@ -1,14 +1,11 @@
 //Стыриный шоп
 
-let PRODUCTS_NAMES = ['Processor', 'Display', 'Notebook', 'Mouse', 'Keyboard', 'Phones', 'Router', 'USB-camera', 'Gamepad']
-let PRICES = [100, 120, 1000, 15, 18, 25, 30, 18, 24]
-let IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-
+/*
 let products = []
 
 function createProductsDTO() {
     let length = IDS.length
-
+    
     for (let i = 0; i < length; i++) {
         products.push(createProduct(i))
     }
@@ -21,7 +18,7 @@ function createProduct(index) {
         id_product: IDS[index]
     }
 }
-createProductsDTO()
+createProductsDTO()*/
 
 //Функционал корзины
 
@@ -33,7 +30,7 @@ createProductsDTO()
 
 
 //Работает после запуска createProductsDTO()
-function countBasketPrice() {
+/*function countBasketPrice() {
     let basket = [...products] //Должен быть в функции пдсчета стоимости
     let sumBasket = 0
     for (let i = 0; i < products.length; i++) { //тут должна быть длина массива корзины
@@ -45,35 +42,40 @@ createProductsDTO()
 
 function basket() {
     let myBasket = []
-
-
+    
+    
 }
-
+*/
 
 //5 Задание ниже
-
+/*
 let myProject = document.querySelector("#container")
 
 function header() {
+    function name(params) {
+        
+    }
+    
+    
     let header = document.createElement("header")
     let logo = document.createElement("a")
     let formSearch = document.createElement("form")
     let inputSearch = document.createElement("input")
     let inputSubmit = document.createElement("input")
     let basketHeade = document.createElement("button")
-
+    
     logo.classList.add("logo")
     formSearch.classList.add("formSearch")
     inputSearch.classList.add("inputSearch")
     inputSubmit.classList.add("inputSubmit")
     basketHeade.classList.add("basketHeader")
-
+    
     logo.setAttribute("href", " ")
     inputSubmit.setAttribute("type", "submit")
     inputSubmit.setAttribute("value", " ")
     logo.innerText = "e-shop"
     basketHeade.innerText = "Корзина"
-
+    
     header.appendChild(logo)
     header.appendChild(basketHeade)
     header.appendChild(formSearch)
@@ -82,8 +84,104 @@ function header() {
     myProject.appendChild(header)
 }
 header()
+*/
 
-function catalog() {
+
+let PRODUCTS_NAMES = ['Processor', 'Display', 'Notebook', 'Mouse', 'Keyboard', 'Phones', 'Router', 'USB-camera', 'Gamepad']
+let PRICES = [100, 120, 1000, 15, 18, 25, 30, 18, 24]
+let IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+function header2() {
+    let myProject = document.querySelector("#container")
+    myProject.innerHTML =
+        `<header class="header">
+    <a class="logo" href=" ">e-shop</a>
+    <button class="basketHeader">Корзина</button>
+    <form class="formSearch">
+    <input class="inputSearch">
+    <input class="inputSubmit" type="submit" value=" ">
+    </form>
+    <div class="basket hiden">
+    <img src="https://placehold.it/200x150">
+            </div>
+        
+        </header>
+        <main class="main-container">
+        </main>`
+
+
+}
+header2()
+
+
+function createProduct(index) {
+    return {
+        product_name: PRODUCTS_NAMES[index],
+        price: PRICES[index],
+        id_product: IDS[index],
+        img: 'https://placehold.it/200x150',
+        createTemplate() {
+            return `
+                <div class="catalog-section" data-id="${this.id_product}">
+                    <img class="img-product" src="${this.img}" alt="${this.product_name}">
+                        <a class="product-href">${this.product_name}</a>
+                        <p class="price"> ${this.price} $ </p>
+                        <button class="buy"
+                            data-id="${this.id_product}"
+                            data-name="${this.product_name}"
+                            data-price="${this.price}">
+                            Купить
+                        </button>
+                       
+                </div>
+            `
+        }
+    }
+}
+let basket = {}
+let catalog = {
+    items: [],
+    container: ".main-container",
+    cart: null,
+
+    init() {
+        this._fetchItems()
+        this._render()
+
+
+        document.querySelector(this.container).addEventListener('click', (evt) => {
+            if (evt.target.classList.contains('buy')) {
+                let product = evt.target.dataset
+                basket.name = createProduct.id_product
+
+            }
+        })
+    },
+
+    _fetchItems() {
+        let length = IDS.length
+
+        for (let i = 0; i < length; i++) {
+            this.items.push(createProduct(i))
+        }
+    },
+    _render() {
+        let container = document.querySelector(this.container)
+        let domString = ''
+
+        this.items.forEach(item => {
+            domString += item.createTemplate()
+        })
+        container.innerHTML = domString
+    }
+}
+
+catalog.init()
+
+
+
+
+/*function catalog() {
     let catalogAmount = PRODUCTS_NAMES.length
     for (let i = 0; i < catalogAmount; i++) {
         let div = document.createElement("div")
@@ -116,4 +214,4 @@ function catalog() {
     }
 }
 
-catalog()
+catalog()*/
